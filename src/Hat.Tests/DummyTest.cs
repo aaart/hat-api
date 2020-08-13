5f23db79d633587ad6663e124f0ea6b1cc416972
artur rachwal <artur.rachwal@gmail.com>
2020-08-13 22:50:56 +0200

    dummy unit test added

--- src/Hat.Tests/DummyTest.cs
+++ src/Hat.Tests/DummyTest.cs
@@ -3,11 +3,12 @@ using Xunit;
 
 namespace Hat.Tests
 {
-    public class UnitTest1
+    public class DummyTest
     {
         [Fact]
-        public void Test1()
+        public void Dummy()
         {
+            Assert.True(true);
         }
     }
 }
