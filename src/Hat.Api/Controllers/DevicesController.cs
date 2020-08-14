54df9bdf1d63d4422d3efc7e352425d1e06cfbfd
artur rachwal <artur.rachwal@gmail.com>
2020-08-14 23:36:58 +0200

    http verbs changed

--- src/Hat.Api/Controllers/DevicesController.cs
+++ src/Hat.Api/Controllers/DevicesController.cs
@@ -4,16 +4,19 @@ namespace Hat.Api.Controllers
 {
     public class DevicesController : Controller
     {
+        [HttpGet]
         public IActionResult All()
         {
             return Ok();
         }
 
+        [HttpPut]
         public IActionResult UpdateState()
         {
             return NoContent();
         }
 
+        [HttpPatch]
         public IActionResult ChangeState()
         {
             return Accepted();
