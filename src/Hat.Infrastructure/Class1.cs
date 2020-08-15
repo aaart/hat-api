74bce9446060b2fa6ea0cbf527b615928ec57a5b
artur rachwal <artur.rachwal@gmail.com>
2020-08-15 22:52:22 +0200

    HatApiController added mvc dependencies added

--- src/Hat.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hat.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Hat.Infrastructure/Mvc/HatApiController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hat.Infrastructure.Mvc
+{
+    public class HatApiController : ControllerBase
+    {
+        
+    }
+}
