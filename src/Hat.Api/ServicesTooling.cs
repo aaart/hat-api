c76f3d7d618c08f4bd1f627be460913c54bc1f3e
artur rachwal <artur.rachwal@gmail.com>
2020-08-31 22:09:27 +0200

    services tooling added

---
+++ src/Hat.Api/ServicesTooling.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Reflection;
+using Hat.Domain.Devices.Services;
+using Hat.Infrastructure.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Hat.Api
+{
+    public static class ServicesTooling
+    {
+        public static void RegisterDomainServices(this IServiceCollection services)
+        {
+            var assembly = typeof(GetDevicesService).Assembly;
+            var domainServices = 
+                assembly
+                    .GetTypes()
+                    .Where(t => t.BaseType != null && t.BaseType.IsGenericType && t.BaseType.GetGenericTypeDefinition() == typeof(BaseService<,>));
+
+            foreach (var domainService in domainServices)
+            {
+                var contract = domainService.GetInterfaces().Single(i => !i.IsGenericType);
+                services.AddScoped(contract, domainService);
+            }
+        }
+    }
+}
