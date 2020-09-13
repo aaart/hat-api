cdcd3c50cd7ea90a9c7e1cd18639774ab08d98f3
artur rachwal <artur.rachwal@gmail.com>
2020-09-13 21:50:05 +0200

    bugfix

--- src/Hat.Infrastructure/Mvc/Swagger/MixedRouteBodyFilter.cs
+++ src/Hat.Infrastructure/Mvc/Swagger/MixedRouteBodyFilter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using Hat.Infrastructure.Service;
 using HybridModelBinding;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -12,35 +13,46 @@ namespace Hat.Infrastructure.Mvc.Swagger
         public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
             var parameters = context.MethodInfo.GetParameters();
-            var attrDefined = parameters.Count(x => x.GetCustomAttribute(typeof(FromHybridAttribute)) != null) == 1;
-            if (attrDefined)
+            var parameter = parameters.FirstOrDefault(x => x.GetCustomAttribute(typeof(FromHybridAttribute)) != null);
+            if (parameter != null && IsDerivedFromBaseGenericDto(parameter.ParameterType))
             {
-                var parameter = parameters[0];
-                if (parameter.ParameterType.BaseType != null && parameter.ParameterType.BaseType.IsGenericType)
+                var idProperty = parameter.ParameterType.GetProperties().First(p => p.Name == "Id");
+                operation.Parameters.Clear();
+                operation.Parameters.Add(new OpenApiParameter
                 {
-                    operation.Parameters.Clear();
-                    operation.Parameters.Add(new OpenApiParameter
-                    {
-                        Name = parameter.ParameterType.BaseType.GetProperties().First().Name.ToLower(),
-                        Required = true,
-                        Schema = new OpenApiSchema { Type = "integer" },
-                        In = ParameterLocation.Path
-                    });
+                    Name = idProperty.Name.ToLower(),
+                    Required = true,
+                    Schema = new OpenApiSchema { Type = idProperty.PropertyType == typeof(int) ? "integer" : idProperty.PropertyType.Name },
+                    In = ParameterLocation.Path
+                });
 
-                    operation.RequestBody = new OpenApiRequestBody();
-                    var parameterSchema = context.SchemaRepository.Schemas[parameter.ParameterType.Name];
-                    var paramaterSchemaNoId = new OpenApiSchema
-                    {
-                        Properties = parameterSchema.Properties
-                            .Where(x => !string.Equals("id", x.Key, StringComparison.InvariantCultureIgnoreCase))
-                            .ToDictionary(x => x.Key, x => x.Value)
-                    };
-                    operation.RequestBody.Content.Add("application/json", new OpenApiMediaType
-                    {
-                        Schema = paramaterSchemaNoId
-                    });
-                }
+                operation.RequestBody = new OpenApiRequestBody();
+                var parameterSchema = context.SchemaRepository.Schemas[parameter.ParameterType.Name];
+                var paramaterSchemaNoId = new OpenApiSchema
+                {
+                    Properties = parameterSchema.Properties
+                        .Where(x => !string.Equals("id", x.Key, StringComparison.InvariantCultureIgnoreCase))
+                        .ToDictionary(x => x.Key, x => x.Value)
+                };
+                operation.RequestBody.Content.Add("application/json", new OpenApiMediaType
+                {
+                    Schema = paramaterSchemaNoId
+                });
             }
         }
+        
+        public static bool IsDerivedFromBaseGenericDto(Type type)
+        {
+            while (type.BaseType != null)
+            {
+                if (type.BaseType.IsConstructedGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(Dto<>))
+                {
+                    return true;
+                }
+
+                type = type.BaseType;
+            }
+            return false;
+        } 
     }
 }
